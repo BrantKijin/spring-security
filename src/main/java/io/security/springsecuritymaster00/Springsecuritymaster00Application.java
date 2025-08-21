2ad07d7a5345ccad313a7b988f8c5912dcc183f3
곽기진 <upper0house@gmail.com>
2025-08-21 14:56:28 +0900

    Setup Spring Boot project with Gradle and basic structure

---
+++ src/main/java/io/security/springsecuritymaster00/Springsecuritymaster00Application.java
@@ -0,0 +1,13 @@
+package io.security.springsecuritymaster00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsecuritymaster00Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springsecuritymaster00Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/security/springsecuritymaster00/Springsecuritymaster00ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.springsecuritymaster00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springsecuritymaster00ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
